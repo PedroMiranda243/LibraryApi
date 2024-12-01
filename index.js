ce6a61d7589b3cc68ccf651d277cbf04b4c0570f
Rick <ricardo.almeida200315@gmail.com>
2024-12-01 18:17:56 -0300

    merge-maycom_rafa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const routes = express.Router();
 routes.get("/", function (req, res, next) {
-    res.status(200).send("Ok").end();
+  res.status(200).send("Ok").end();
 });
 
 export { routes as default };
